f5408e20329c2e0d34e7a38748a68e3bd640ca7c
chrmn6 <charmainerubiato2017@gmail.com>
2025-10-07 14:37:03 +0800

    changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1527,7 +1527,17 @@
             <nav class="flex items-center justify-end gap-4">
                 @auth
                     @php $user = Auth::user(); @endphp
-                    <a href="{{ $user->role === 'resident' ? route('residents.dashboard') : url('/dashboard') }}"
+                    <a href="
+                                        @if ($user->role === 'admin')
+                                            {{ route('admin.dashboard') }}
+                                        @elseif ($user->role === 'staff')
+                                            {{ route('staff.dashboard') }}
+                                        @elseif ($user->role === 'resident')
+                                            {{ route('residents.dashboard') }}
+                                        @else
+                                            {{ route('dashboard') }}
+                                        @endif
+                                    "
                         class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                         Dashboard
                     </a>
