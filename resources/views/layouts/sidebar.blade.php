c67e41adf1ea9fdfc4f4b498b2a77279c2ec829e
chrmn6 <charmainerubiato2017@gmail.com>
2025-11-26 23:42:04 +0800

    AHHH LAST FOR THIS NOTIFICATION

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -25,22 +25,25 @@
             <div class="flex items-center">
                 <div class="flex items-center ms-3">
                     <div class="relative rounded-full text-gray-600 hover:bg-gray-100 focus:outline-none me-4">
-                        <svg class="w-6 h-6 text-gray-600 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="1.5"
-                                d="m10.827 5.465-.435-2.324m.435 2.324a5.338 5.338 0 0 1 6.033 4.333l.331 1.769c.44 2.345 2.383 2.588 2.6 3.761.11.586.22 1.171-.31 1.271l-12.7 2.377c-.529.099-.639-.488-.749-1.074C5.813 16.73 7.538 15.8 7.1 13.455c-.219-1.169.218 1.162-.33-1.769a5.338 5.338 0 0 1 4.058-6.221Zm-7.046 4.41c.143-1.877.822-3.461 2.086-4.856m2.646 13.633a3.472 3.472 0 0 0 6.728-.777l.09-.5-6.818 1.277Z" />
-                        </svg>
+                        <a href="{{ route('notifications.index') }}">
+                            <svg class="w-6 h-6 text-gray-600 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    d="m10.827 5.465-.435-2.324m.435 2.324a5.338 5.338 0 0 1 6.033 4.333l.331 1.769c.44 2.345 2.383 2.588 2.6 3.761.11.586.22 1.171-.31 1.271l-12.7 2.377c-.529.099-.639-.488-.749-1.074C5.813 16.73 7.538 15.8 7.1 13.455c-.219-1.169.218 1.162-.33-1.769a5.338 5.338 0 0 1 4.058-6.221Zm-7.046 4.41c.143-1.877.822-3.461 2.086-4.856m2.646 13.633a3.472 3.472 0 0 0 6.728-.777l.09-.5-6.818 1.277Z" />
+                            </svg>
 
-                        @php
-                            $unreadCount = Auth::user()->unreadNotifications->count();
-                        @endphp
-                        @if($unreadCount > 0)
-                            <span
-                                class="absolute -top-1 -right-1 bg-red-600 text-white text-xs w-4 h-4 flex items-center justify-center rounded-full">
-                                {{ $unreadCount }}
-                            </span>
-                        @endif
+                            @php
+                                $unreadCount = Auth::user()->unreadNotifications->count();
+                            @endphp
+                            @if($unreadCount > 0)
+                                <span
+                                    class="absolute -top-1 -right-1 bg-red-600 text-white text-xs w-4 h-4 flex items-center justify-center rounded-full">
+                                    {{ $unreadCount }}
+                                </span>
+                            @endif
+                        </a>
                     </div>
                     <div>
                         <button type="button"
