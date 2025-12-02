e1e08b2a46385275fa26646afe6ef20c771b893d
chrmn6 <charmainerubiato2017@gmail.com>
2025-12-03 07:46:41 +0800

    added trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->use([
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
